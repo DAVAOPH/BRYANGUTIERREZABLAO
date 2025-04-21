8f9c64e01bfafd4eff66a7e1f5f1225b2f7ddd88
Bryan G. Ablao <ariadne.annotator356@gmail.com>
2025-04-21 13:54:52 +0800

    Update and rename resume.pdf to scripts.js

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// Toggle Hamburger Menu
+document.addEventListener('DOMContentLoaded', function () {
+  const hamburger = document.getElementById('hamburger');
+  const navMenu = document.getElementById('nav-menu');
+
+  hamburger.addEventListener('click', () => {
+    navMenu.style.display = navMenu.style.display === 'flex' ? 'none' : 'flex';
+  });
+});
