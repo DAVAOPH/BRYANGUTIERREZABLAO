48332a7aca4bb98aa2505589a9666b754a9b9c27
Bryan G. Ablao <ariadne.annotator356@gmail.com>
2025-04-21 14:01:28 +0800

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-// Toggle Hamburger Menu
-document.addEventListener('DOMContentLoaded', function () {
-  const hamburger = document.getElementById('hamburger');
-  const navMenu = document.getElementById('nav-menu');
-
-  hamburger.addEventListener('click', () => {
-    navMenu.style.display = navMenu.style.display === 'flex' ? 'none' : 'flex';
-  });
-});
